dc4ca4ba011e85f15747f7003c6099439a3f2f60
Sebastian Weidmann <sebastian.weidmann.l@gmail.com>
2019-09-13 18:07:10 -0300

    Inicio de validación de login form

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+declare function initPlugins();
 
 @Component({
   selector: 'app-register',
@@ -7,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  forma: FormGroup;
+
   constructor() { }
 
+  igualPass(campo1: string, campo2: string) {
+
+    return (group: FormGroup) => {
+      let pass1 = group.controls[campo1].value;
+      let pass2 = group.controls[campo2].value;
+
+      if (pass1 === pass2) {
+        return null;
+      } else {
+        return { igualPass: true };
+      }
+    }
+  }
+
   ngOnInit() {
-    
+    initPlugins();
+
+    this.forma = new FormGroup({
+      nombre: new FormControl(null, Validators.required),
+      correo: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required),
+      password2: new FormControl(null, Validators.required),
+      condiciones: new FormControl(false)
+    }, { validators: this.igualPass('password', 'password2') });
+  }
+
+  registrarUsuario() {
+    console.log(this.forma.valid);
+    console.log(this.forma.value);
   }
 
 }
